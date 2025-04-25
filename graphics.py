93237ef06b3abc25b2da4d42590020f1e2165a9f
Gushi <agusescoriza@outlook.es>
2025-04-25 10:58:25 -0300

    Lines and points with drawing

--- graphics.py
+++ graphics.py
@@ -14,6 +14,9 @@ class Window:
         self.__root.update_idletasks()
         self.__root.update()
 
+    def draw_line(self, line, fill_color):
+        line.draw(self.__canvas, fill_color)
+
     def wait_for_close(self):
         self.__running = True
         while self.__running:
@@ -22,3 +25,16 @@ class Window:
 
     def close(self):
         self.__running = False
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    
+class Line:
+    def __init__(self, point1, point2):
+        self.__point1 = point1
+        self.__point2 = point2
+    
+    def draw(self, canvas: Canvas, fill_color):
+        canvas.create_line(self.__point1.x, self.__point1.y, self.__point2.x, self.__point2.y, fill=fill_color, width=2)
--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-from graphics import Window
+from graphics import Window, Line, Point
 
 def main():
     win = Window(800, 600)
-    win.wait_for_close()
 
+    line1 = Line(Point(50, 100), Point(0, 75))
+    line2 = Line(Point(66, 33), Point(100, 75))
+    win.draw_line(line1, "black")
+    win.draw_line(line2, "black")
+    win.wait_for_close()
 
 main()
